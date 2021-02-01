4aa617fdd91b853543dcd5d6690214c0f783ecfc
森賢一 <wanitokotori11@gmail.com>
2021-02-01 16:43:21 +0900

    staff登録(ログイン・ログアウト)

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-    with_options presence: true do
+  with_options presence: true do
     validates :nickname
     validates :email, allow_blank: true
     validates :password, allow_blank: true, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'は半角英文字で入力してください', allow_blank: true }
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
   root to: "pages#index"
-  devise_for :users
+
+  # ユーザー
+  devise_for :users, controllers: {
+    sessions:      'users/sessions',
+    passwords:     'users/passwords',
+    registrations: 'users/registrations'
+  }
+
+  # スタッフ
+  devise_for :staffs, controllers: {
+    sessions: 'staffs/sessions',
+    passwords: 'staffs/passwords',
+    registrations: 'staffs/registrations'
+  }
 end
